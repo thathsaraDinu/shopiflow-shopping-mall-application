d78173ccd8d98372f9d4e8b0ed12d4c4cd9d90c1
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-08-07 19:37:12 +0530

    chore: Update .env file with VITE_SERVER_URL and optimize Prettier config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import AppRoutes from './routes/AppRoutes';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          position="left"
+          buttonPosition="bottom-left"
+        />
+      </QueryClientProvider>
     </>
   );
 }
