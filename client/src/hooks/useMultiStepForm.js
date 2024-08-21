fbbc0662c32ef487b58c95b6db00893b36741208
thathsaraDinu <wthathsaradinuwan@gmail.com>
2024-08-21 10:10:22 +0530

    Reapply "addpromotionfront"
    This reverts commit 8fe8cba6adff79f0a51d40c56e75e5a681092599.

---
+++ client/src/hooks/useMultiStepForm.js
@@ -0,0 +1,36 @@
+import { ReactElement, useState } from 'react';
+export function useMultistepForm(steps) {
+  const [currentStepIndex, setCurrentStepIndex] =
+    useState(0);
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i > steps.length - 1) return i;
+      return i + 1;
+    });
+  }
+
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  }
+
+  function goTo(index) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    isFirstStep: currentStepIndex === 0,
+    isSecondStep: currentStepIndex === 1,
+    isLastStep: currentStepIndex === steps.length - 1,
+    isComplete: currentStepIndex === steps.length,
+    goTo,
+    next,
+    back,
+  };
+}
