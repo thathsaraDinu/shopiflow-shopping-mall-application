5161000deb4cd6e2c47ac847470b30653485816e
NadeesH <spnadeeshnimsara@gmail.com>
2024-08-28 13:30:20 +0530

    Create product model

---
+++ server/src/models/product.model.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from 'mongoose';
+
+export const ProductSchema = new Schema(
+  {
+    productID: {
+      type: String,
+      required: [true, 'Product ID is required'],
+      unique: true
+    },
+    name: {
+      type: String,
+      required: [true, 'Product name is required'],
+      unique: false
+    },
+    category: {
+      type: String,
+      required: [true, 'Product category is required'],
+      unique: false
+    },
+    buyingPrice: {
+      type: Number,
+      required: [true, 'Product buying price is required'],
+      unique: false
+    },
+    quantity: {
+      type: Number,
+      required: [true, 'Product quantity is required'],
+      unique: false
+    },
+    unitPrice: {
+      type: Number,
+      required: [true, 'Product unit price is required'],
+      unique: false
+    },
+    supplier: {
+      type: String,
+      required: [true, 'Product supplier is required'],
+      unique: false
+    },
+    thresholdValue: {
+      type: Number,
+      required: [true, 'Product thresholdValue is required'],
+      unique: false
+    }
+  },
+  {
+    timestamps: { currentTime: () => Date.now() + 5.5 * 60 * 60 * 1000 }
+  }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
