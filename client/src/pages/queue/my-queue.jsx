e0866ef2c962127c6505e9d80f1e823c60d3679e
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-08-31 14:36:07 +0530

    feat: Add getUserQueues API function and implement MyQueue page
    This commit adds a new API function `getUserQueues` to the `queue.api.js` file, which fetches the queues associated with the current user. It also implements the `MyQueue` page, which displays the user's joined queues using the `QueueCard` component.
    
    These changes enhance the application by allowing users to view their joined queues and improve the user experience by providing a personalized queue management feature.

--- client/src/pages/queue/my-queue.jsx
+++ client/src/pages/queue/my-queue.jsx
@@ -1,5 +1,41 @@
-const MyQueue = () => {
-  return <div>My Queue</div>;
-};
+import { getUserQueues } from '@/api/queue.api';
+import { useQuery } from '@tanstack/react-query';
+import QueueCard from '@/components/queue-card/queue-card';
 
+const MyQueue = () => {
+  // Get the shop's queues
+  const {
+    data: queues,
+    isLoading: queuesLoading,
+    isError: queuesError,
+  } = useQuery({
+    queryKey: ['queues'],
+    queryFn: () => getUserQueues(),
+  });
+
+  return (
+    <div>
+      {queuesLoading && <p>Loading...</p>}
+      {queuesError && <p>Error fetching queues</p>}
+      <div className="p-5">
+        <div className="flex flex-row justify-between">
+          <h2 className="text-2xl font-bold">
+            Joind Queues
+          </h2>
+        </div>
+        {queues && (
+          <div className="mt-5 grid grid-cols-1 gap-4 justify-items-center">
+            {queues.map((queue, index) => (
+              <QueueCard
+                key={queue._id}
+                queue={queue}
+                index={index}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 export default MyQueue;
