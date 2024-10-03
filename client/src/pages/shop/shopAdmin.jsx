8fc62fec507dcd76c845b6adbbfd1d0a682b4d15
Eshan Nayanapriya <161910749+Eshan-Nayanapriya@users.noreply.github.com>
2024-10-03 15:36:53 +0530

    Update shopAdmin.jsx

--- client/src/pages/shop/shopAdmin.jsx
+++ client/src/pages/shop/shopAdmin.jsx
@@ -197,63 +197,73 @@ const ShopAdmin = () => {
             <h1 className='text-center mt-6 mb-6 text-4xl font-medium' style={{ fontFamily: 'Bebas Neue, sans-serif' }}>Available Shops</h1>
             {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
-            <Link
-                to={'/dashboard/addshop'}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-5 mt-2"
-            >
-                Add Shop
-            </Link>
-            <Button
-                onClick={generateReport}
-                className="bg-green-500 text-white font-bold py-2 px-4 rounded ml-5 mt-2"
-            >
-                Generate Report
-            </Button>
+            <div className="flex justify-end mr-5 mt-2">
+
+                <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                    <Link
+                        to={'/dashboard/addshop'}
+                        style={{ padding: '10px 20px' }}
+                    >
+                        Add Shop
+                    </Link>
+                </Button>
+                <Button
+                    onClick={generateReport}
+                    className="bg-green-500 text-white font-bold py-2 px-4 rounded ml-2"
+                    style={{ padding: '10px 20px' }}
+                >
+                    Generate Report
+                </Button>
+            </div>
+
+
 
             {/* Display Shops */}
-            {shops.map((shop) => (
-                <div
-                    key={shop._id}
-                    className="m-5 p-4 border rounded shadow"
-                >
-                    <div className='mt-2 mb-4'>
-                        <h2 className="text-lg font-medium" style={{ fontFamily: 'Righteous, sans-serif', fontSize: '30px' }}>
-                            {shop.name}
-                        </h2>
-                    </div>
-                    <p className="text-base text-gray-600">
-                        Location: {shop.location}
-                    </p>
-                    <p className="text-base text-gray-600">
-                        Open Time: {shop.openTime}
-                    </p>
-                    <p className="text-base text-gray-600">
-                        Contact Number: {shop.contactNumber}
-                    </p>
-                    {/* Display newly added fields */}
-                    <p className="text-base text-gray-600">
-                        Owner Email: {shop.ownerEmail}
-                    </p>
-                    <p className="text-base text-gray-600">
-                        Shop Type: {shop.shopType}
-                    </p>
-                    <Link
-                        to={`/dashboard/updateshop/${shop._id}`}
+            {
+                shops.map((shop) => (
+                    <div
+                        key={shop._id}
+                        className="m-5 p-4 border rounded shadow"
                     >
-                        <Button className="bg-green hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-2 ml-0">
-                            Update Shop
-                        </Button>
-                    </Link>
+                        <div className='mt-2 mb-4'>
+                            <h2 className="text-lg font-medium" style={{ fontFamily: 'Righteous, sans-serif', fontSize: '30px' }}>
+                                {shop.name}
+                            </h2>
+                        </div>
+                        <p className="text-base text-gray-600">
+                            Location: {shop.location}
+                        </p>
+                        <p className="text-base text-gray-600">
+                            Open Time: {shop.openTime}
+                        </p>
+                        <p className="text-base text-gray-600">
+                            Contact Number: {shop.contactNumber}
+                        </p>
+                        {/* Display newly added fields */}
+                        <p className="text-base text-gray-600">
+                            Owner Email: {shop.ownerEmail}
+                        </p>
+                        <p className="text-base text-gray-600">
+                            Shop Type: {shop.shopType}
+                        </p>
+                        <Link
+                            to={`/dashboard/updateshop/${shop._id}`}
+                        >
+                            <Button className="bg-green hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-2 ml-0">
+                                Update Shop
+                            </Button>
+                        </Link>
 
-                    <Button
-                        onClick={() => handleDeleteShop(shop._id)}
-                        className="bg-red hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-2 ml-2"
-                    >
-                        Delete Shop
-                    </Button>
-                </div>
-            ))}
-        </div>
+                        <Button
+                            onClick={() => handleDeleteShop(shop._id)}
+                            className="bg-red hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-2 ml-2"
+                        >
+                            Delete Shop
+                        </Button>
+                    </div>
+                ))
+            }
+        </div >
     );
 };
 
