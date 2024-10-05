ff1d99f4752876291c673ea306d4026205543667
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-10-05 20:12:30 +0530

    Refactor queue history page and add download report button

--- client/src/pages/queue/history.jsx
+++ client/src/pages/queue/history.jsx
@@ -1,7 +1,49 @@
+import { getQueues } from '@/api/queue.api';
+import { useShopStore } from '@/store/shop-store';
+import { useQuery } from '@tanstack/react-query';
+import { Button } from '@/components/ui/button';
+import { QUEUE_STATUS } from '@/constants';
+import toast from 'react-hot-toast';
+import QueueTable from './queue-table';
+
 const History = () => {
+  const shopId = useShopStore((state) => state.shopId);
+
+  // Fetch the queue data
+  const {
+    data: queues,
+    isLoading: queuesLoading,
+    error: queuesError,
+  } = useQuery({
+    queryKey: ['queues-history'],
+    queryFn: () =>
+      getQueues(shopId, [
+        QUEUE_STATUS.COMPLETED,
+        QUEUE_STATUS.CANCELLED,
+      ]),
+  });
+
   return (
     <div>
-      <h1>History</h1>
+      <div className="flex justify-between items-center">
+        {/* Report Download Button */}
+        <div className="flex justify-end">
+          <Button
+            onClick={() => {
+              toast.success('Report downloaded');
+            }}
+          >
+            Download Report
+          </Button>
+        </div>
+      </div>
+      <div className="mt-4">
+        <QueueTable
+          queues={queues}
+          isLoading={queuesLoading}
+          isError={queuesError}
+        />
+      </div>
     </div>
   );
 };
