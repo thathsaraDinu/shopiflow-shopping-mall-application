7c79a9f9dea38b2fa7209a151017edfe5cb926e7
thathsaraDinu <wthathsaradinuwan@gmail.com>
2025-01-04 17:29:40 +0530

    update server configuration for development environment and enhance logger setup

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = SERVER_PORT;
 const corsOptions = {
-  origin: 'http://localhost:5173', // Specify your frontend URL
+  origin: process.env.NODE_ENV, // Specify your frontend URL
   credentials: true // Allow cookies to be sent
 };
 
